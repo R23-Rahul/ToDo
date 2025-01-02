3406fa4a7ae422501d2c29e9ab483a555597d1ea
R23-Rahul <rahulchejara123@gmail.com>
2025-01-02 13:55:22 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Configure the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Define the ToDo model
+class ToDo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+# Define the home route
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=ToDo(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo=ToDo.query.all()
+    
+    return render_template('index.html',allTodo=allTodo)
+
+# Create the database tables before running the app
+
+@app.route('/About')
+def About():
+    return render_template('About.html')
+
+@app.route('/update/<int:sno>', methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo = ToDo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = ToDo.query.filter_by(sno=sno).first()
+    
+    return render_template('update.html',todo=todo)
+
+
+#CODE FOR DELETING A TODOAPP
+@app.route('/delete/<int:sno>', methods=['GET'])
+def delete(sno):
+    print(f"Deleting todo with sno: {sno}")  # This helps in debugging
+    todo = ToDo.query.filter_by(sno=sno).first()
+    if todo:  # Ensure the todo exists
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect("/")
+
+
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Ensures tables are created
+    app.run(debug=True)
